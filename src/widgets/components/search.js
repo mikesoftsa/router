c866b9c1e3c6841fae60f2c8c561d26c58acc10a
Miguel Triana <mikesoft241184@gmail.com>
2018-12-06 00:29:53 -0500

    013 013 Prompt validación antes de dejar la página

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './search.css';
+import { Prompt } from 'react-router';
 
 const Search = (props) => (
     <form
         className="Search"
         onSubmit={props.handleSubmit}
     >
+        <Prompt
+            when={props.prompt}
+            message="Estas seguro de querer dejar la pagina?"
+        />
         <input
             ref={props.setRef}
             type="text"
